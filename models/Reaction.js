83d144c51c3f89c169ce2edcc1abeb311fd4bb59
kmart134 <ksmgps@hotmail.com>
2022-08-16 15:20:13 -0400

    connection.js file updated

--- models/Reaction.js
+++ models/Reaction.js
@@ -32,3 +32,6 @@ const reactionSchema = new Schema(
 );
 
 module.exports = reactionSchema;
+
+//This will not be a model, but rather will be used as the reaction field's subdocument schema in the Thought model.
+//schema ooonlyyyy*
