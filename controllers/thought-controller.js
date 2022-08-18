5f04783309373c9210e1a22a0379221758e5011c
kmart134 <ksmgps@hotmail.com>
2022-08-17 20:41:21 -0400

    deleteThought updated

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -60,6 +60,30 @@ updateThought(req, res) {
     //Thought.findOneandRemove
     //also need User.findOneAndUpdate - use $pull to pull the thought from the user's thought's array
 
+deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+        .then((thought) =>
+        !thought
+            ? res.status(404).json({ message: 'No such thought exists' })
+            : User.findOneAndUpdate(
+                {thoughts: req.params.thoughtId},
+                { $pull: { thoughts: req.params.thoughtId } },
+                { new: true }
+            )
+        )
+        .then((user) =>
+        !user
+            ? res.status(404).json({
+                message: 'Thought deleted, but no user found',
+            })
+            : res.json({ message: 'Thought successfully deleted' })
+        )
+        .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+    },
+
 //add a reaction to athought
     //Thought.findOneandUpdate
     //$addToSet
